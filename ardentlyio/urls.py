f0d815399f421cbad3dc1f077b09622db948084a
Daniel Mruk <daniel.mruk1@gmail.com>
2017-03-30 00:08:08 -0400

    sign up page

--- ardentlyio/urls.py
+++ ardentlyio/urls.py
@@ -1,21 +1,12 @@
-"""ardentlyio URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
+from ardentlyioapp import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^stream/sign-in/$', auth_views.login, {'template_name': 'stream/sign_in.html'}, name = 'stream-sign-in'),
+    url(r'^stream/sign-out', auth_views.logout, {'next_page': '/'}, name = 'stream-sign-out'),
+    url(r'^stream/$', views.stream_home, name = 'stream-home'),
 ]
--- ardentlyioapp/views.py
+++ ardentlyioapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def home(request):
+    return redirect(stream_home)
+
+@login_required(login_url='/stream/sign-in/')
+
+def stream_home(request):
+    return render(request, 'stream/home.html')
